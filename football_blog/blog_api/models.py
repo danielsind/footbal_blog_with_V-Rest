235237f68e3d530aea532bfd04d40829b8387312
danielsind <danielsindet@gmail.com>
2023-06-09 09:36:36 +0300

    Added an ImageField to the Post model called post_image

--- football_blog/blog_api/models.py
+++ football_blog/blog_api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    post_image = models.ImageField(default='default.jpg', upload_to='post_image_pics')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
