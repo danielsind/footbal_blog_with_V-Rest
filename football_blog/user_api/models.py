769ec9aa23444c96c83bf47bcd4deed2a0bad42a
danielsind <danielsindet@gmail.com>
2023-06-09 09:40:24 +0300

    Defined a function to save the image field for the profile, defined a function to aadjust the size of big images, added a string function to return the username of a profile user

--- football_blog/user_api/models.py
+++ football_blog/user_api/models.py
@@ -7,3 +7,17 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+def __str__(self):
+    return f'{self.username} Profile'
+
+def save(self, *args, **kwargs):
+    super(Profile).save(*args, **kwargs)
+
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(self.image.path)
