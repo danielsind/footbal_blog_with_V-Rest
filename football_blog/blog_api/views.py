9ca8eb4ca7969f4a9d5db83a66ea94685de6aa5b
danielsind <danielsindet@gmail.com>
2023-06-09 10:46:58 +0300

    Defined the post_list method to allow a user to get and post a Post object, and it is saved, added a decorator @api_view to allow the api to be viewed as well

--- football_blog/blog_api/views.py
+++ football_blog/blog_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Post
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from football_blog.serializers import PostSerializer
 
-# Create your views here.
+
+@api_view (['GET', 'POST'])
+def post_list(request, format=None):
+
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
