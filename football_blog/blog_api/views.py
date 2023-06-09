c0a3f2dc20841eed9b4e2d7dd8d88d43b800a14f
danielsind <danielsindet@gmail.com>
2023-06-09 11:34:42 +0300

    defined a post_detail functions for the GET,PUT,DELETE functions and defined all conditions for the three functions, to the DELETE POST

--- football_blog/blog_api/views.py
+++ football_blog/blog_api/views.py
@@ -19,3 +19,27 @@ def post_list(request, format=None):
         if serializer.is_valid:
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, id, format=None):
+
+    try:
+       post = Post.objects.get(pk=id)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
