fac98bedfe0ec4dc3be7faf99f6177faafe96dc0
danielsind <danielsindet@gmail.com>
2023-06-12 10:04:18 +0300

    Edited ProfileSerializer ti UserProfile Serializer, and added the category field to PostSerializer

--- football_blog/serializers.py
+++ football_blog/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 from blog_api.models import Post
-from user_api.models import Profile
+from user_api.models import UserProfile
+from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'author','post_image']
+        fields = ['id', 'title', 'content', 'author','post_image','category']
 
-class ProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Profile
-        field = ['id', 'user','image']
+        model = UserProfile
+        field = ['id', 'user','image',]
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ['id', 'username', 'email']
