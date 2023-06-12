5679dd5c132570728457d5536e97a1c071d1364a
danielsind <danielsindet@gmail.com>
2023-06-12 12:21:45 +0300

    deleted unnescecary imports, and edited the as_view()

--- football_blog/blog_api/urls.py
+++ football_blog/blog_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .import views
-from blog_api import views
+from blog_api import views as blog_api_views
 from user_api import views as user_api_views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('posts/', views.post_list),
-    path('posts/<int:id>', views.post_detail)
+    path('posts/', blog_api_views.PostAPIView.as_view(), name='posts'),
+    path('posts/<int:id>', blog_api_views.PostDetailView.as_view(), name='post-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
