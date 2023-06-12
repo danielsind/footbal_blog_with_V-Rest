c660500a013c47bd34902f45615ce56e4a0a3b88
danielsind <danielsindet@gmail.com>
2023-06-12 12:20:42 +0300

    Validated image and videos formats, also ensured that a user will only post a video or image but cant do both

--- football_blog/blog_api/models.py
+++ football_blog/blog_api/models.py
@@ -6,16 +6,23 @@ from PIL import Image
 
 
 def validate_video_file(value):
-    # Custom validation to ensure the uploaded file is a video
-    # You can implement your own validation logic here
-    # For example, check the file extension or use external libraries
-
-    # Sample validation using file extension
     allowed_extensions = ['.mp4', '.avi', '.mov']
     if not value.name.lower().endswith(tuple(allowed_extensions)):
         raise ValidationError("Only video files are allowed.")
-# Create your models here.
+
+def validate_image_format(file):
+    try:
+        image = Image.open(file)
+
+        allowed_formats = ['JPEG', 'PNG', 'GIF', 'JPG']
+        if image.format not in allowed_formats:
+            raise models.ValidationError("Invalid image format. Only JPEG, PNG, and GIF are allowed.")
+
+    except Exception:
+        raise models.ValidationError("Unable to open the image file.")
+
 class Post(models.Model):
+
     CATEGORY_CHOICES = (
         ('general', 'General'),
         ('leagues', 'Leagues'),
@@ -23,13 +30,27 @@ class Post(models.Model):
         ('latest news', 'Latest News'),
         ('videos', 'Videos'),
     )
+
+    IMAGE = 'image'
+    VIDEO = 'video'
+    CHOICES = (
+        (IMAGE, 'Image'),
+        (VIDEO, 'Video'),
+    )
+
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    post_image = models.ImageField(default='default.jpg', upload_to='post_image_pics')
+    post_image = models.ImageField(default='default.jpg', upload_to='post_image_pics', validators=[validate_image_format], null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='general')
-    video_file = models.FileField(upload_to='videos/',validators=[validate_video_file], default='default_video.mp4')
+    video_file = models.FileField(upload_to='videos/',validators=[validate_video_file], default='default_video.mp4',  null=True, blank=True)
+
+    def clean(self):
+        # Ensure that only one field (image or video) is filled
+        if self.image and self.video:
+            raise ValidationError("Only one of image or video should be provided.")
 
     def __str__(self):
             return self.title
