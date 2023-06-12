3f5e7a283e307ce7f40fccd566448960d5960b78
danielsind <danielsindet@gmail.com>
2023-06-12 10:01:23 +0300

    Edited the Profile model to UserProfile

--- football_blog/user_api/models.py
+++ football_blog/user_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image 
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
@@ -13,7 +13,7 @@ def __str__(self):
     return f'{self.username} Profile'
 
 def save(self, *args, **kwargs):
-    super(Profile).save(*args, **kwargs)
+    super(UserProfile).save(*args, **kwargs)
 
     img = Image.open(self.image.path)
 
