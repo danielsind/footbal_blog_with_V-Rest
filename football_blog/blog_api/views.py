a26f0387086b246438c26785bb60d9c0db337480
danielsind <danielsindet@gmail.com>
2023-06-12 11:11:24 +0300

    Edited the PostDetailView to ensure actions done on a post by a user have permisions reagarding PUT,DELETE operatios

--- football_blog/blog_api/views.py
+++ football_blog/blog_api/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from .models import Post
-from django.db.models import Q
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework import status
 from serializers import PostSerializer
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from .permissions import IsOwnerOrReadOnly
 
 class PostAPIView(APIView):
     def get(self, request: Request):
@@ -21,29 +22,41 @@ class PostAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PostDetailAPIView(APIView):
-    def get_object(self, pk):
+class PostDetailView(APIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_post(self, post_id):
         try:
-            return Post.objects.get(pk=pk)
+            return Post.objects.get(id=post_id)
         except Post.DoesNotExist:
             raise status.HTTP_404_NOT_FOUND
 
-    def get(self, request: Request, pk):
-        instance = self.get_object(pk)
-        serializer = PostSerializer(instance)
+    def get(self, request, post_id):
+        post = self.get_post(post_id)
+        serializer = PostSerializer(post)
         return Response(serializer.data)
 
-    def put(self, request: Request, pk):
-        instance = self.get_object(pk)
-        serializer = PostSerializer(instance, data=request.data)
+    def put(self, request, post_id):
+        post = self.get_post(post_id)
+        
+        # Check if the user is the owner of the post
+        if post.user != request.user:
+            raise PermissionDenied("You do not have permission to update this post.")
+
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request: Request, pk):
-        instance = self.get_object(pk)
-        instance.delete()
+    def delete(self, request, post_id):
+        post = self.get_post(post_id)
+        
+        # Check if the user is the owner of the post
+        if post.user != request.user:
+            raise PermissionDenied("You do not have permission to delete this post.")
+
+        post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 class FilteredPostsAPIView(APIView):
