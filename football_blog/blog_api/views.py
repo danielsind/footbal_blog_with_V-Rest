7bf79e926d19287a3e6308dce3e36bcdb9b7cd97
danielsind <danielsindet@gmail.com>
2023-06-12 10:08:10 +0300

    Created a view for GET,POST,PUT,DELETE, a view for category view, a filter class for a search querry

--- football_blog/blog_api/views.py
+++ football_blog/blog_api/views.py
@@ -1,46 +1,75 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework import status
 from serializers import PostSerializer
 
-
-@api_view (['GET', 'POST'])
-def post_list(request, format=None):
-
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
+class PostAPIView(APIView):
+    def get(self, request: Request):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
-    
-    if request.method == 'POST':
+
+    def post(self, request: Request): 
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, id, format=None):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    try:
-       post = Post.objects.get(pk=id)
-    except Post.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = PostSerializer(post)
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+
+    def get(self, request: Request, pk):
+        instance = self.get_object(pk)
+        serializer = PostSerializer(instance)
         return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        serializer = PostSerializer(post, data=request.data)
 
+    def put(self, request: Request, pk):
+        instance = self.get_object(pk)
+        serializer = PostSerializer(instance, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def delete(self, request: Request, pk):
+        instance = self.get_object(pk)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class FilteredPostsAPIView(APIView):
+    def get(self, request):
+        queryset = Post.objects.all()
+
+        # Apply filters
+        title = request.query_params.get('title')
+        author = request.query_params.get('author')
+        content = request.query_params.get('content')
+        category = request.query_params.get('category')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+        if content:
+            queryset = queryset.filter(category__icontains=content)
+        if category:
+            queryset = queryset.filter(category__icontains=category)
+
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class CategoryPostsAPIView(APIView):
+    def get(self, request: Request, category: str):
+        posts = Post.objects.filter(category=category)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
